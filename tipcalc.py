df7fee77851247fcd6241a66d365e53375cf5604
Shahir Ali <shahirsheikali@gmail.com>
2020-09-02 15:49:40 -0700

    Update tipcalc.py

--- tipcalc.py
+++ tipcalc.py
@@ -1,3 +1,8 @@
 # This is my calucaltor project based on calculating the tip for a meal.
+# The sales tax in CA is 7.25
 print('Hello')
 print('I will help you caluclate the tip for that delicious meal you just devoured :)')
+
+meal = float(input("Enter your meal amount: "))
+tip = int(input("Enter your tip %: "))
+tax = 7.25
