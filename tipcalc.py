30b9027b3e850c094b9f2a9eff9e9fbf259ef01b
Shahir Ali <shahirsheikali@gmail.com>
2020-09-02 16:39:29 -0700

    Update tipcalc.py

--- tipcalc.py
+++ tipcalc.py
@@ -5,4 +5,12 @@ print('I will help you caluclate the tip for that delicious meal you just devour
 
 meal = float(input("Enter your meal amount: "))
 tip = int(input("Enter your tip %: "))
-tax = 7.25
+tax = 0.7
+
+# Calculations
+tip_amt = meal * tip/100
+tax_amt = meal * tax
+total = meal + tip_amt + tax_amt
+
+print(f"Your meal was ${meal:.2f} and your tip was ${tip_amt:.2f}.")
+print(f"Your total with tax was ${total:.2f}.")
