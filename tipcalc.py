e812251c59c8786cb4ad887f16716c60b9e324b2
Shahir Ali <shahirsheikali@gmail.com>
2020-09-02 16:47:28 -0700

    Update tipcalc.py

--- tipcalc.py
+++ tipcalc.py
@@ -12,5 +12,5 @@ tip_amt = meal * tip/100
 tax_amt = meal * tax
 total = meal + tip_amt + tax_amt
 
-print(f"Your meal was ${meal:.2f} and your tip was ${tip_amt:.2f}.")
-print(f"Your total with tax was ${total:.2f}.")
+print(f"\nYour meal was ${meal:.2f} and your tip was ${tip_amt:.2f}.")
+print (f"Your total with tax was ${total:.2f}." + "PS. Dont forget to wear your masks, and tip generously.\n")
